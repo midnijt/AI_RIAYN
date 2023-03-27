c385756603150cff8de8b2c17b1a0477f5f2e794
Midnijt <92066831+midnijt@users.noreply.github.com>
2023-03-27 12:45:44 -0400

    Update to only work on a single batch

--- code/data/augmentation.py
+++ code/data/augmentation.py
@@ -13,12 +13,12 @@ class CutOut1D:
         :param data: Input 1D vector (PyTorch tensor)
         :return: Augmented 1D vector
         """
-        data_length = len(data)
+        data_length = data.size(0)
         mask = torch.ones(data_length, dtype=torch.float32)
 
         for _ in range(self.n_holes):
-            indices = torch.randint(0, data_length, (self.length,))
-            mask[indices] = 0.0
+            indices = torch.randint(0, data_length - self.length + 1, (1,))
+            mask[indices : indices + self.length] = 0.0
 
         return data * mask
 
@@ -27,34 +27,34 @@ class MixUp1D:
     def __init__(self, alpha):
         self.alpha = alpha
 
-    def __call__(self, data1, data2):
+    def __call__(self, data):
         """
-        Applies the Mix-Up augmentation to a pair of input 1D vectors.
+        Applies the Mix-Up augmentation to an input batch of 1D vectors.
 
-        :param data1: First input 1D vector (PyTorch tensor)
-        :param data2: Second input 1D vector (PyTorch tensor)
-        :return: Augmented 1D vector
+        :param data: Input batch of 1D vectors (PyTorch tensor)
+        :return: Augmented batch of 1D vectors
         """
-        assert len(data1) == len(data2), "Input vectors must have the same length."
-
-        lam = torch.distributions.Beta(self.alpha, self.alpha).sample()
-        return lam * data1 + (1 - lam) * data2
+        data2 = data[torch.randperm(data.size(0))]
+        lam = torch.distributions.Beta(self.alpha, self.alpha).sample(
+            (data.size(0), 1, 1)
+        )
+        return lam * data + (1 - lam) * data2
 
 
 class CutMix1D:
     def __init__(self, alpha):
         self.alpha = alpha
 
-    def __call__(self, data1, data2):
+    def __call__(self, data):
         """
-        Applies the Cut-Mix augmentation to a pair of input 1D vectors.
+        Applies the Cut-Mix augmentation to an input batch of 1D vectors.
 
-        :param data1: First input 1D vector (PyTorch tensor)
-        :param data2: Second input 1D vector (PyTorch tensor)
-        :return: Augmented 1D vector
+        :param data: Input batch of 1D vectors (PyTorch tensor)
+        :return: Augmented batch of 1D vectors
         """
-        assert len(data1) == len(data2), "Input vectors must have the same length."
-
-        lam = torch.distributions.Beta(self.alpha, self.alpha).sample()
-        mask = torch.bernoulli(torch.full_like(data1, lam))
-        return data1 * mask + data2 * (1 - mask)
+        data2 = data[torch.randperm(data.size(0))]
+        lam = torch.distributions.Beta(self.alpha, self.alpha).sample(
+            (data.size(0), 1, 1)
+        )
+        mask = torch.bernoulli(torch.full((data.size(0), data.size(1), 1), lam))
+        return data * mask + data2 * (1 - mask)
