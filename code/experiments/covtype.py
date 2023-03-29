4f9c8a0ab97e78ad829b79c2610dfc719aad205a
Midnijt <92066831+midnijt@users.noreply.github.com>
2023-03-29 13:56:55 -0400

    fix 0 index error

--- code/experiments/covtype.py
+++ code/experiments/covtype.py
@@ -4,7 +4,7 @@ from model_training import train_and_evaluate_model
 
 def main():
     dataset = fetch_covtype()
-    y = dataset.target - 1  # zero-index the labels
+    dataset.target -= 1  # zero-index the labels
     train_and_evaluate_model(dataset, is_regression=False)
 
 
