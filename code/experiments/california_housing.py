f6220d32d76c2c607b38c16a4dbd4af1d01bdf86
Midnijt <92066831+midnijt@users.noreply.github.com>
2023-03-29 12:32:42 -0400

    XGBoost now adapted for regression

---
+++ code/experiments/california_housing.py
@@ -0,0 +1,63 @@
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import sys
+
+sys.path.append("/Users/jtam/projects/AI_RIAYN/code/")
+from model.NeuralNet import NeuralNet
+from model.xgboost import XGBoost
+import model.search_space as search_space
+
+
+def main():
+    # Load dataset
+    dataset = fetch_california_housing()
+    X = dataset.data
+    y = dataset.target
+
+    # Split data into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    # Scale data
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    # Train and evaluate MLP
+    mlp = NeuralNet(
+        n_inputs=X_train.shape[1],
+        n_outputs=1,  # regression
+        search_space=search_space.mlp,
+        n_layers=2,
+        n_hidden_units=64,
+    )
+
+    mlp_params = mlp.hyperparameter_tuning(
+        X_train=X_train,
+        y_train=y_train,
+        X_val=X_test,
+        y_val=y_test,
+        n_iterations=1,
+    )
+
+    mse = mlp.evaluate(X_test, y_test)
+    print(f"MLP mean squared error: {mse:.2f}")
+    print("Best MLP parameters:", mlp_params)
+
+    # Train and evaluate XGBoost
+    xgb = XGBoost(num_class=1, search_space=search_space.xgb)
+
+    xgb_params = xgb.hyperparameter_tuning(
+        X_train=X_train, y_train=y_train, X_val=X_test, y_val=y_test
+    )
+
+    mse = xgb.evaluate(X_test, y_test)
+    print(f"XGBoost mean squared error: {mse:.2f}")
+    print("Best XGBoost parameters:", xgb_params)
+
+
+if __name__ == "__main__":
+    main()
--- code/model/xgboost.py
+++ code/model/xgboost.py
@@ -1,35 +1,55 @@
-import numpy as np
-from sklearn.metrics import accuracy_score
-from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score, mean_squared_error
+from xgboost import XGBClassifier, XGBRegressor
 from bayes_opt import BayesianOptimization
 from .base import BaseModel
 
 
 class XGBoost(BaseModel):
-    def __init__(self, search_space):
+    def __init__(self, num_class, search_space):
+        self.num_class = num_class
+        self.regression = num_class == 1
         self.search_space = search_space
         self.model = None
 
     def build_model(self, **params):
-        self.model = XGBClassifier(
-            learning_rate=params["eta"],
-            reg_lambda=params["lambda"],
-            reg_alpha=params["alpha"],
-            n_estimators=int(params["num_round"]),
-            gamma=params["gamma"],
-            colsample_bylevel=params["colsample_bylevel"],
-            colsample_bynode=params["colsample_bynode"],
-            colsample_bytree=params["colsample_bytree"],
-            max_depth=int(params["max_depth"]),
-            max_delta_step=int(params["max_delta_step"]),
-            min_child_weight=params["min_child_weight"],
-            subsample=params["subsample"],
-            objective="multi:softmax",
-            num_class=10,
-            random_state=42,
-            n_jobs=-1,
-            early_stopping_rounds=10,
-        )
+        if self.regression:
+            self.model = XGBRegressor(
+                learning_rate=params["eta"],
+                reg_lambda=params["lambda"],
+                reg_alpha=params["alpha"],
+                n_estimators=int(params["num_round"]),
+                gamma=params["gamma"],
+                colsample_bylevel=params["colsample_bylevel"],
+                colsample_bynode=params["colsample_bynode"],
+                colsample_bytree=params["colsample_bytree"],
+                max_depth=int(params["max_depth"]),
+                max_delta_step=int(params["max_delta_step"]),
+                min_child_weight=params["min_child_weight"],
+                subsample=params["subsample"],
+                random_state=42,
+                n_jobs=-1,
+                early_stopping_rounds=10,
+            )
+        else:
+            self.model = XGBClassifier(
+                learning_rate=params["eta"],
+                reg_lambda=params["lambda"],
+                reg_alpha=params["alpha"],
+                n_estimators=int(params["num_round"]),
+                gamma=params["gamma"],
+                colsample_bylevel=params["colsample_bylevel"],
+                colsample_bynode=params["colsample_bynode"],
+                colsample_bytree=params["colsample_bytree"],
+                max_depth=int(params["max_depth"]),
+                max_delta_step=int(params["max_delta_step"]),
+                min_child_weight=params["min_child_weight"],
+                subsample=params["subsample"],
+                objective="multi:softmax",
+                num_class=self.num_class,
+                random_state=42,
+                n_jobs=-1,
+                early_stopping_rounds=10,
+            )
 
     def fit(self, X_train, y_train, X_val, y_val, params):
         self.build_model(**params)
@@ -43,5 +63,9 @@ class XGBoost(BaseModel):
 
     def evaluate(self, X, y):
         y_pred = self.model.predict(X)
-        acc = accuracy_score(y, y_pred)
-        return acc
+        if self.regression:
+            metric = mean_squared_error(y, y_pred)
+        else:
+            # smaller is better
+            metric = -accuracy_score(y, y_pred)
+        return metric
